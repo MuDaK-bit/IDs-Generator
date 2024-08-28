806234d8e98cddb5e68e7920b24d031b06adf335
Professional <Professional@USER-PC>
2024-08-29 01:00:56 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonGenerateListfile = new System.Windows.Forms.Button();
+            this.buttonGeneratePatch = new System.Windows.Forms.Button();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonFindPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonGenerateListfile
+            // 
+            this.buttonGenerateListfile.Location = new System.Drawing.Point(12, 146);
+            this.buttonGenerateListfile.Name = "buttonGenerateListfile";
+            this.buttonGenerateListfile.Size = new System.Drawing.Size(102, 37);
+            this.buttonGenerateListfile.TabIndex = 0;
+            this.buttonGenerateListfile.Text = "Generate listfile";
+            this.buttonGenerateListfile.UseVisualStyleBackColor = true;
+            this.buttonGenerateListfile.Click += new System.EventHandler(this.buttonGenerateListfile_Click);
+            // 
+            // buttonGeneratePatch
+            // 
+            this.buttonGeneratePatch.Location = new System.Drawing.Point(262, 146);
+            this.buttonGeneratePatch.Name = "buttonGeneratePatch";
+            this.buttonGeneratePatch.Size = new System.Drawing.Size(102, 37);
+            this.buttonGeneratePatch.TabIndex = 1;
+            this.buttonGeneratePatch.Text = "Generate patch";
+            this.buttonGeneratePatch.UseVisualStyleBackColor = true;
+            this.buttonGeneratePatch.Click += new System.EventHandler(this.buttonGeneratePatch_Click);
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(37, 38);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(297, 20);
+            this.textBoxID.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Initial ID";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(37, 89);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(297, 20);
+            this.textBoxPath.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(142, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Path to files";
+            // 
+            // buttonFindPath
+            // 
+            this.buttonFindPath.Location = new System.Drawing.Point(340, 89);
+            this.buttonFindPath.Name = "buttonFindPath";
+            this.buttonFindPath.Size = new System.Drawing.Size(22, 19);
+            this.buttonFindPath.TabIndex = 6;
+            this.buttonFindPath.Text = "...";
+            this.buttonFindPath.UseVisualStyleBackColor = true;
+            this.buttonFindPath.Click += new System.EventHandler(this.buttonFindPath_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 206);
+            this.Controls.Add(this.buttonFindPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.buttonGeneratePatch);
+            this.Controls.Add(this.buttonGenerateListfile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "IDs Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerateListfile;
+        private System.Windows.Forms.Button buttonGeneratePatch;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonFindPath;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGeneratePatch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Parse ID
+                int id = int.Parse(textBoxID.Text);
+
+              
+                string folderPath = textBoxPath.Text;
+                if (!Directory.Exists(folderPath))
+                {
+                    MessageBox.Show("Path dont exist!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+
+
+                // Get files from path
+                string[] files = Directory.GetFiles(folderPath);
+
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                    saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string listfilePath = saveFileDialog.FileName;
+
+       
+                        using (StreamWriter writer = new StreamWriter(listfilePath, false))
+                        {
+                            foreach (string file in files)
+                            {
+                    
+                                string fileName = Path.GetFileName(file);
+
+      
+                                string line = $"{{\"file\":\"{fileName}\",\"id\":{id}}},";
+                                writer.WriteLine(line);
+
+                                // Adding to ID 1 
+                                id++;
+                            }
+                        }
+
+                        MessageBox.Show("List succesfully generated!", "Luck", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+      
+                MessageBox.Show($"Got error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void buttonGenerateListfile_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Parse ID
+                int id = int.Parse(textBoxID.Text);
+
+                // Check path
+                string folderPath = textBoxPath.Text;
+                if (!Directory.Exists(folderPath))
+                {
+                    MessageBox.Show("Path dont exist!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+
+
+                // Get all files
+                string[] files = Directory.GetFiles(folderPath);
+
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+
+
+                    saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                    saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        string listfilePath = saveFileDialog.FileName;
+
+
+                        using (StreamWriter writer = new StreamWriter(listfilePath, false))
+                        {
+                            foreach (string file in files)
+                            {
+                                // Get FileName with small letters
+                                string fileName = Path.GetFileName(file).ToLower();
+
+     
+                                string line = $"{id};world/wmo/klanxaoc/doodads/{fileName}";
+                                writer.WriteLine(line);
+
+                                // Adding to ID 1
+                                id++;
+                            }
+                        }
+
+                        MessageBox.Show("List succesfully generated!", "Luck", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+   
+                MessageBox.Show($"Got Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonFindPath_Click(object sender, EventArgs e)
+        {
+
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Choose files folder";
+                folderBrowserDialog.ShowNewFolderButton = true;
+
+                
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBoxPath.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+       
+    }
+}
